365bfdd15015201b630e95882d1e652dd7e6dcef
melanholy <pavelkosara@Pavels-iMac.local>
2018-03-06 08:13:26 -0800

    add relevantTo field and finish (de)serialalization

--- notebook.playground/Contents.swift
+++ notebook.playground/Contents.swift
@@ -7,22 +7,35 @@ enum Importance: Int {
 }
 
 struct Note {
+    private static let defaultRelevantInterval: TimeInterval = 7 * 24 * 60 * 60
+    
     let uid: String
     let title: String
     let content: String
     let color: UIColor
     let importance: Importance
-    
+    let relevantTo: Date
+
     init(uid: String = UUID().uuidString,
          title: String,
          content: String,
          color: UIColor = UIColor.white,
-         importance: Importance) {
+         importance: Importance,
+         relevantTo: Date? = nil) {
         self.uid = uid
         self.title = title
         self.content = content
         self.color = color
         self.importance = importance
+        if let relevantTo = relevantTo {
+            self.relevantTo = relevantTo
+        } else {
+            self.relevantTo = Date().addingTimeInterval(Note.defaultRelevantInterval)
+        }
+    }
+    
+    func isRelevant() -> Bool {
+        return Date() < relevantTo
     }
 }
 
@@ -32,10 +45,16 @@ extension Note {
             var result: [String: Any] = [
                 "uid": uid,
                 "title": title,
-                "content": content
+                "content": content,
+                "relevantTo": relevantTo.timeIntervalSince1970
             ]
+            
             if importance != Importance.common {
                 result["importance"] = importance.rawValue
+            }
+            
+            if color != UIColor.white {
+                result["color"] = color.cgColor.components
             }
             
             return result
@@ -45,20 +64,43 @@ extension Note {
     static func parse(json: [String: Any]) -> Note? {
         guard let uid = json["uid"] as? String,
               let title = json["title"] as? String,
-              let content = json["content"] as? String else {
+              let content = json["content"] as? String,
+              let relevantTo = json["relevantTo"] as? TimeInterval else {
             return nil
         }
         
         let importanceEntry = json["importance"] as? Int
-        var importance: Importance = Importance.common
-        if (importanceEntry != nil) {
-            guard let value = Importance(rawValue: importanceEntry!) else {
+        var importance = Importance.common
+        if let importanceEntry = importanceEntry {
+            guard let value = Importance(rawValue: importanceEntry) else {
                 return nil
             }
             importance = value
         }
         
-        return Note(uid: uid, title: title, content: content, color: UIColor.white, importance: importance)
+        let colorEntry = json["color"] as? [CGFloat]
+        var color = UIColor.white
+        if let colorEntry = colorEntry {
+            if colorEntry.count == 2 {
+                color = UIColor(white: colorEntry[0], alpha: colorEntry[1])
+            } else if colorEntry.count == 4 {
+                color = UIColor(
+                    red: colorEntry[0],
+                    green: colorEntry[1],
+                    blue: colorEntry[2],
+                    alpha: colorEntry[3])
+            } else {
+                return nil
+            }
+        }
+        
+        return Note(
+            uid: uid,
+            title: title,
+            content: content,
+            color: color,
+            importance: importance,
+            relevantTo: Date(timeIntervalSince1970: relevantTo))
     }
 }
 
@@ -136,8 +178,8 @@ class FileNotebook {
         var loadedNotes = [Note]()
         for obj in jsonArray {
             let note = Note.parse(json: obj)
-            if note != nil {
-                loadedNotes.append(note!)
+            if let note = note {
+                loadedNotes.append(note)
             }
         }
         
@@ -145,17 +187,22 @@ class FileNotebook {
     }
 }
 
-let note1 = Note(title: "article", content: "good times!", color: UIColor.black, importance: Importance.high)
+print(FileNotebook.filePath!)
+
+let note1 = Note(title: "article", content: "good times!", color: UIColor.brown, importance: Importance.high)
 let note2 = Note(uid: "23432", title: "news", content: "bad times :(", importance: Importance.common)
+let note3 = Note(title: "oooo", content: "aaaa", importance: Importance.low, relevantTo: Date())
 
 let notebook1 = FileNotebook()
 notebook1.addNote(note: note1)
 notebook1.addNote(note: note2)
-print(FileNotebook.filePath!)
+notebook1.addNote(note: note3)
 try! notebook1.save()
 
 let notebook2 = FileNotebook()
 try! notebook2.load()
+
+print(notebook1.notes)
 print(notebook2.notes)
 
 

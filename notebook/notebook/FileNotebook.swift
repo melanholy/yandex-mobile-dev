3ab77c18af539cfb64fc4f6291dc4f66c5a667c7
user <user@hanky-osx2.local>
2018-03-07 20:58:08 +0500

    add other project files

--- notebook/notebook/FileNotebook.swift
+++ notebook/notebook/FileNotebook.swift
@@ -6,4 +6,86 @@
 //  Copyright © 2018 user. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class FileNotebook {
+    private static let storeFilename = "notes.json"
+    
+    private(set) var notes = [Note]()
+    
+    static var filePath: String? {
+        guard let dir = NSSearchPathForDirectoriesInDomains(
+            .documentDirectory,
+            .allDomainsMask,
+            true).first else {
+                return nil
+        }
+        
+        let path = "\(dir)/\(storeFilename)"
+        
+        return path
+    }
+    
+    func addNote(note: Note) -> Bool {
+        if notes.contains(where: { (existingNote) -> Bool in
+            return existingNote.uid == note.uid
+        }) {
+            return false
+        }
+        
+        notes.append(note)
+        
+        return true
+    }
+    
+    func removeNote(withUid uid: String) -> Bool {
+        guard let index = notes.index(where: { (note) -> Bool in
+            return note.uid == uid
+        }) else {
+            return false
+        }
+        
+        notes.remove(at: index)
+        
+        return true
+    }
+    
+    func save() throws {
+        guard let filePath = FileNotebook.filePath else {
+            return
+        }
+        
+        let jsonNotes = notes.map { (note) -> [String: Any] in
+            return note.json
+        }
+        
+        let data = try! JSONSerialization.data(withJSONObject: jsonNotes, options: [])
+        guard let jsonString = String(data: data, encoding: .utf8) else {
+            return
+        }
+        
+        try! jsonString.write(toFile: filePath, atomically: false, encoding: .utf8)
+    }
+    
+    func load() throws {
+        guard let filePath = FileNotebook.filePath else {
+            return
+        }
+        
+        let content = try! String(contentsOfFile: filePath)
+        guard let data = content.data(using: .utf8),
+            let jsonArray = try! JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] else {
+                return
+        }
+        
+        var loadedNotes = [Note]()
+        for obj in jsonArray {
+            let note = Note.parse(json: obj)
+            if let note = note {
+                loadedNotes.append(note)
+            }
+        }
+        
+        notes = loadedNotes
+    }
+}
--- notebook/notebook/Note.swift
+++ notebook/notebook/Note.swift
@@ -6,4 +6,108 @@
 //  Copyright © 2018 user. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum Importance: Int {
+    case low = 0
+    case common = 1
+    case high = 2
+}
+
+struct Note {
+    private static let defaultRelevantInterval: TimeInterval = 7 * 24 * 60 * 60
+    
+    let uid: String
+    let title: String
+    let content: String
+    let color: UIColor
+    let importance: Importance
+    let relevantTo: Date
+    
+    init(uid: String = UUID().uuidString,
+         title: String,
+         content: String,
+         color: UIColor = UIColor.white,
+         importance: Importance,
+         relevantTo: Date? = nil) {
+        self.uid = uid
+        self.title = title
+        self.content = content
+        self.color = color
+        self.importance = importance
+        if let relevantTo = relevantTo {
+            self.relevantTo = relevantTo
+        } else {
+            self.relevantTo = Date().addingTimeInterval(Note.defaultRelevantInterval)
+        }
+    }
+    
+    func isRelevant() -> Bool {
+        return Date() < relevantTo
+    }
+}
+
+extension Note {
+    var json: [String: Any] {
+        get {
+            var result: [String: Any] = [
+                "uid": uid,
+                "title": title,
+                "content": content,
+                "relevantTo": relevantTo.timeIntervalSince1970
+            ]
+            
+            if importance != Importance.common {
+                result["importance"] = importance.rawValue
+            }
+            
+            if color != UIColor.white {
+                result["color"] = color.cgColor.components
+            }
+            
+            return result
+        }
+    }
+    
+    static func parse(json: [String: Any]) -> Note? {
+        guard let uid = json["uid"] as? String,
+            let title = json["title"] as? String,
+            let content = json["content"] as? String,
+            let relevantTo = json["relevantTo"] as? TimeInterval else {
+                return nil
+        }
+        
+        let importanceEntry = json["importance"] as? Int
+        var importance = Importance.common
+        if let importanceEntry = importanceEntry {
+            guard let value = Importance(rawValue: importanceEntry) else {
+                return nil
+            }
+            importance = value
+        }
+        
+        let colorEntry = json["color"] as? [CGFloat]
+        var color = UIColor.white
+        if let colorEntry = colorEntry {
+            if colorEntry.count == 2 {
+                color = UIColor(white: colorEntry[0], alpha: colorEntry[1])
+            } else if colorEntry.count == 4 {
+                color = UIColor(
+                    red: colorEntry[0],
+                    green: colorEntry[1],
+                    blue: colorEntry[2],
+                    alpha: colorEntry[3])
+            } else {
+                return nil
+            }
+        }
+        
+        return Note(
+            uid: uid,
+            title: title,
+            content: content,
+            color: color,
+            importance: importance,
+            relevantTo: Date(timeIntervalSince1970: relevantTo))
+    }
+}
